7322f76680e4588d7e1175258462d0ba1db1da77
erikka4052 <09124055@cumt.edu.cn>
2024-01-07 22:15:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jane', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '打人工，百广爱好者(特指qsxy)，科技小白', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.nanshi.dedyn.io', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jane's Blog,所思所感, 科技分享', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jane Blog,所思所感, 科技分享', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'janesameen119@protonmail.com', // 邮箱地址 例如mail@tangly1024.com
