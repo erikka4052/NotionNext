98fec57f98ad8ead68d79898d718acb9f4945b4a
erikka4052 <09124055@cumt.edu.cn>
2024-02-27 17:16:44 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,17 +26,13 @@ class MyDocument extends Document {
                       return <link key={index} rel="preload" href={fontUrl} as="font" type="font/woff2" />
                     }
                   })}
-                <script dangerouslySetInnerHTML={{
-                  __html: `
+ 				        <script type="text/javascript">
                     (function(c,l,a,r,i,t,y){
-                      c[a] = c[a] || function(){(c[a].q=c[a].q||[]).push(arguments)};
-                      t=l.createElement(r);
-                      t.async=1;
-                      t.src="https://www.clarity.ms/tag/"+i;
-                        y=l.getElementsByTagName(r)[0];
-                        y.parentNode.insertBefore(t, y);
-                   })(window, document,"clarity","script","l874upe8wc")`
-                }}/>
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "l874upe8wc")
+                </script>
 
                 </Head>
 
