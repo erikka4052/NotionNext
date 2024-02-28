6d3ca28c2ec3dc6909a203e2163c800af94432a8
erikka4052 <09124055@cumt.edu.cn>
2024-02-28 16:23:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -21,13 +21,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script type="text/javascript">
+        <script dangerouslySetInnerHTML={{
+          __html: `
             (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "l874upe8wc");
-        </script>
+              c[a] = c[a] || function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+           })(window, document,"clarity","script","l874upe8wc")`
+        }}/>
       </Head>
 
     </>
